dbaec1a3b21051a867c8d6588e25946d0b1906d6
NickPeut <veropeu@mail.ru>
2022-03-05 02:09:54 +0500

    lesson 5

--- src/lesson5/KtOpenAddressingSet.kt
+++ src/lesson5/KtOpenAddressingSet.kt
@@ -1,9 +1,14 @@
 package lesson5
 
+import lesson6.NoughtsAndCrosses
+import java.util.*
+
 /**
  * Множество(таблица) с открытой адресацией на 2^bits элементов без возможности роста.
  */
 class KtOpenAddressingSet<T : Any>(private val bits: Int) : AbstractMutableSet<T>() {
+    private val DELETE = Any()
+
     init {
         require(bits in 2..31)
     }
@@ -51,7 +56,7 @@ class KtOpenAddressingSet<T : Any>(private val bits: Int) : AbstractMutableSet<T
         val startingIndex = element.startingIndex()
         var index = startingIndex
         var current = storage[index]
-        while (current != null) {
+        while (current != null && current != DELETE) {
             if (current == element) {
                 return false
             }
@@ -75,8 +80,21 @@ class KtOpenAddressingSet<T : Any>(private val bits: Int) : AbstractMutableSet<T
      *
      * Средняя
      */
+
+    // Производительность O(capacity)
+    // Ресурсоемкость O(1)
     override fun remove(element: T): Boolean {
-        TODO("not implemented")
+        val startingIndex = element.startingIndex()
+        var index = startingIndex
+        do {
+            if (storage[index] == element) {
+                storage[index] = DELETE
+                size--
+                return true
+            }
+            index = (index + 1) % capacity
+        } while (index != startingIndex)
+        return false
     }
 
     /**
@@ -89,7 +107,40 @@ class KtOpenAddressingSet<T : Any>(private val bits: Int) : AbstractMutableSet<T
      *
      * Средняя (сложная, если поддержан и remove тоже)
      */
-    override fun iterator(): MutableIterator<T> {
-        TODO("not implemented")
+    override fun iterator(): MutableIterator<T> = OpenAddressingSetIterator()
+
+    private inner class OpenAddressingSetIterator : MutableIterator<T> {
+        var index = 0
+        var elementsFound = 0
+        var numberOfElements = size
+        var lastNext: Any? = null
+
+        // Производительность O(1)
+        // Ресурсоемкость O(1)
+        override fun hasNext(): Boolean {
+            return elementsFound < numberOfElements
+        }
+
+        // Производительность O(capacity)
+        // Ресурсоемкость O(1)
+        override fun next(): T {
+            if (elementsFound == numberOfElements) throw NoSuchElementException()
+            lastNext = null
+            while (lastNext == null || lastNext == DELETE) {
+                lastNext = storage[index]
+                index++
+            }
+            elementsFound++
+            return lastNext as T
+        }
+
+        // Производительность O(1)
+        // Ресурсоемкость O(1)
+        override fun remove() {
+            if (lastNext == null) throw IllegalStateException()
+            storage[index - 1] = DELETE
+            size--
+            lastNext = null
+        }
     }
 }
